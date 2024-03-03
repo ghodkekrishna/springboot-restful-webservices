ba1b5781efb0cfd8c7d82bdc16474efcf0fe72d9
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-02 18:35:33 -0800

    Add API validation

--- src/main/java/net/codefusionhub/springboot/controller/UserController.java
+++ src/main/java/net/codefusionhub/springboot/controller/UserController.java
@@ -1,5 +1,6 @@
 package net.codefusionhub.springboot.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.codefusionhub.springboot.dto.UserDto;
 import net.codefusionhub.springboot.service.UserService;
@@ -18,7 +19,7 @@ public class UserController {
 
     // build create User REST API
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
@@ -43,7 +44,7 @@ public class UserController {
     @PutMapping("{id}")
     // http://localhost:8080/api/users/1
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
-                                           @RequestBody UserDto user){
+                                           @RequestBody @Valid UserDto user){
         user.setId(userId);
         UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
--- src/main/java/net/codefusionhub/springboot/dto/UserDto.java
+++ src/main/java/net/codefusionhub/springboot/dto/UserDto.java
@@ -1,5 +1,7 @@
 package net.codefusionhub.springboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+
+    @NotEmpty(message = "Please enter valid firstname.")
     private String firstName;
+
+    @NotEmpty(message = "Please enter valid lastname.")
     private String lastName;
+
+    @NotEmpty(message = "Email field should not be empty.")
+    @Email(message = "Please enter valid email.")
     private String email;
 }
